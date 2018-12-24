58a173af60b4f6f34f0d9da2f2a6564cc3d5f397
Juandi-M <juanmonge95@gmail.com>
2018-12-24 14:38:20 -0600

    Se finaliza la calcauladora

--- TestCalculator/Equations.cs
+++ TestCalculator/Equations.cs
@@ -3,36 +3,80 @@ using System.IO;
 
 namespace TestCalculator
 {
-    class Calculator
+    class Equations
     {
-        private int a;
-        private int b;
-        private int c;
-
+        private double a;
+        private double b;
+        private double result;
         public void Suma()
         {
             try
             {
-                int a = Console.R
+                Console.WriteLine("Escriba el valor de a");
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Escriba el valor de b");
+                b = Convert.ToInt32(Console.ReadLine());
+
+                result = a + b;
+                Console.WriteLine("Su resultado es: "+ result);
             }
             catch (IOException e)
             {
-
+                Console.WriteLine("Erro solo usar numeros.");
             }
-           
+
         }
         public void Resta()
         {
+            try
+            {
+                Console.WriteLine("Escriba el valor de a");
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Escriba el valor de b");
+                b = Convert.ToInt32(Console.ReadLine());
 
+                result = a - b;
+                Console.WriteLine("Su resultado es: " + result);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Erro solo usar numeros.");
+            }
         }
 
         public void Multiplicacion()
         {
-
+            try
+            {
+                Console.WriteLine("Escriba el valor de a");
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Escriba el valor de b");
+                b = Convert.ToInt32(Console.ReadLine());
+                result = a * b;
+                Console.WriteLine("Su resultado es: " + result);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Erro solo usar numeros.");
+            }
         }
 
         public void Division()
         {
+            try
+            {
+                Console.WriteLine("Escriba el valor de a");
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Escriba el valor de b");
+                b = Convert.ToInt32(Console.ReadLine());
+
+                result = a / b;
+                Console.WriteLine("Su resultado es: " + result);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Erro solo usar numeros.");
+            }
 
         }
     }
--- TestCalculator/Program.cs
+++ TestCalculator/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TestCalculator
 {
@@ -10,7 +6,35 @@ namespace TestCalculator
     {
         static void Main(string[] args)
         {
+            Equations myInstance = new Equations();
 
+            Console.WriteLine("Bienvenido a la calculadora de 2 digitos");
+            Console.WriteLine("Seleccione 1 para Suma");
+            Console.WriteLine("Seleccione 2 para Resta");
+            Console.WriteLine("Seleccione 3 para Mulitplicación");
+            Console.WriteLine("Seleccione 4 para División");
+            Console.WriteLine("");
+
+
+
+            int caseSwitch = Int32.Parse(Console.ReadLine());
+
+            switch (caseSwitch)
+            {
+                case 1:
+                    myInstance.Suma();
+                    break;
+                case 2:
+                    myInstance.Resta();
+                    break;
+                case 3:
+                    myInstance.Multiplicacion();
+                    break;
+                case 4:
+                    myInstance.Division();
+                    break;
+            }
+            Console.ReadLine();
         }
     }
 }
