356809b4c0c04936ff01e2f0719e3d1e83781f0a
Juandi-M <juanmonge95@gmail.com>
2018-12-24 11:51:46 -0600

    First update del codigo mas git setup

---
+++ TestCalculator/Equations.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace TestCalculator
+{
+    class Calculator
+    {
+        private int a;
+        private int b;
+        private int c;
+
+        public void Suma()
+        {
+            try
+            {
+                int a = Console.R
+            }
+            catch (IOException e)
+            {
+
+            }
+           
+        }
+        public void Resta()
+        {
+
+        }
+
+        public void Multiplicacion()
+        {
+
+        }
+
+        public void Division()
+        {
+
+        }
+    }
+}
